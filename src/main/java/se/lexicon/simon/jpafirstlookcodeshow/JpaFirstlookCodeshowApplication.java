ec20f2341a9dff12ecbb9dfd99dc386e9644e6ee
Simon Elbrink <Simon.Elbrink@gmail.com>
2020-08-11 10:51:39 +0200

    Initial commit

---
+++ src/main/java/se/lexicon/simon/jpafirstlookcodeshow/JpaFirstlookCodeshowApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.simon.jpafirstlookcodeshow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaFirstlookCodeshowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaFirstlookCodeshowApplication.class, args);
+	}
+
+}
